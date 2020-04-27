94eb1d7a283e8dc82c043846a1f36e4bc1fb05df
Jason Ray <user@users-Air.lan>
2020-04-27 16:42:00 -0700

    added inital files

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './components/App.module.css';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div className={styles.container}>
+                <Cards />
+                <Chart />
+                <CountryPicker />
+            </div>
+        )
+    }
+}
+
+export default App;
